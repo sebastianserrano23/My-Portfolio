2e2f426338a9435c28f0067985771feea2146a65
Sebastian <serranosebastian23@gmail.com>
2023-06-20 08:59:29 -0700

    updates, need to start commiting more often, I have no idea exactly what I'm commiting

--- thebrogammer/src/routes/+layout.svelte
+++ thebrogammer/src/routes/+layout.svelte
@@ -1,11 +1,14 @@
 <script lang='ts'>
 	// The ordering of these imports is critical to your app working properly
-	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+	import '@skeletonlabs/skeleton/themes/theme-rocket.css';
 	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
-	import { AppShell, AppBar, LightSwitch, Drawer, drawerStore } from '@skeletonlabs/skeleton';
+	import { AppShell, AppBar, LightSwitch, Drawer, drawerStore, popup, storePopup } from '@skeletonlabs/skeleton';
+	import type { PopupSettings } from '@skeletonlabs/skeleton';
+	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
+	storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
 	function openSideBar(): void {
 		drawerStore.open();
 	}
@@ -19,22 +22,24 @@
 			<svelte:fragment slot="lead"><LightSwitch /></svelte:fragment>
 				<strong class="text-xl uppercase">The Brogrammer</strong>
 			<svelte:fragment slot="trail">
-				<a href="https://discord.gg/EXqV7W8MtY" 
-					target="_blank" 
-					rel="noreferrer">
+				<button class="btn variant-filled">
 					Discord
-				</a>
-				<a href="https://www.linkedin.com/in/sebastian-serrano-a11a57198?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BasHmyvdlQW%2BJYuuByYcy4A%3D%3D"
-					target="_blank"
-					rel="noreferrer">
-					LinkedIn
-				</a>
-				<a href="https://github.com/sebastianserrano23/"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a></svelte:fragment>
+				</button>
+				<button class="btn variant-filled">
+					<a href="https://www.linkedin.com/in/sebastian-serrano-a11a57198?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BasHmyvdlQW%2BJYuuByYcy4A%3D%3D"
+						target="_blank"
+						rel="noreferrer">
+						LinkedIn
+					</a>
+				</button>
+				<button class="btn variant-filled">
+					<a href="https://github.com/sebastianserrano23/"
+						target="_blank"
+						rel="noreferrer">
+						GitHub
+					</a>
+				</button>
+			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
 	<!-- Page Route Content -->
